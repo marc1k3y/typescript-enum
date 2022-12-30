25f41efef5df3cb7824154ab0a6bd55a7f149af9
marc1k3y <marc1k3y@gmail.com>
2022-12-30 12:13:29 +0300

    init

---
+++ enum.js
@@ -0,0 +1,9 @@
+// Typescript.enum()
+"use strict";
+var Directions;
+(function (Directions) {
+  Directions[Directions["Up"] = 0] = "Up";
+  Directions[Directions["Down"] = 1] = "Down";
+  Directions[Directions["Left"] = 2] = "Left";
+  Directions[Directions["Right"] = 3] = "Right";
+})(Directions || (Directions = {}));
